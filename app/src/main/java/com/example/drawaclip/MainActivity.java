ac127e26eab160a346ba5534bbc81ce80b46e50c
65187002 <abdulrahmann2@educbe.ca>
2022-12-14 10:09:30 -0700

    third commit, some stuff not working tho

--- app/src/main/java/com/example/drawaclip/MainActivity.java
+++ app/src/main/java/com/example/drawaclip/MainActivity.java
@@ -29,10 +29,15 @@ import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.kyanogen.signatureview.SignatureView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.Date;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
 
@@ -58,9 +63,36 @@ public class MainActivity extends AppCompatActivity {
         imgEraser = findViewById(R.id.btnEraser);
         imgSave = findViewById(R.id.btnSave);
 
-        //askPermission();
+        askPermission();
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+        String date = format.format(new Date());
+        fileName = path + "/" + date + ".png";
+
+        if(!path.exists()){
+            path.mkdirs();
+        }
 
         defaultColor = ContextCompat.getColor(MainActivity.this, R.color.black);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                txtPenSize.setText(progress + "dp");
+                signatureView.setPenSize(progress);
+                seekBar.setMax(50);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         imgColor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +100,40 @@ public class MainActivity extends AppCompatActivity {
                 openColorPicker();
             }
         });
+
+        imgEraser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signatureView.clearCanvas();
+            }
+        });
+
+        imgSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!signatureView.isBitmapEmpty()){
+                    try{
+                        saveImage();
+                    } catch (IOException e){
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "Couldn't save painting!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+    private void saveImage() throws IOException {
+        File file = new File(fileName);
+
+        Bitmap bitmap = signatureView.getSignatureBitmap();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos);
+        byte[] bitmapData = bos.toByteArray();
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(bitmapData);
+        fos.flush();
+        fos.close();
+        Toast.makeText(this, "Painting Saved!", Toast.LENGTH_SHORT).show();
     }
 
     private void openColorPicker(){
@@ -87,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
         ambilWarnaDialog.show();
     }
 
-/*
+
     private void askPermission(){
         Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE).withListener(new MultiplePermissionsListener() {
             @Override
@@ -96,11 +162,11 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken){
+            public void onPermissionRationaleShouldBeShown(List<com.karumi.dexter.listener.PermissionRequest> list, PermissionToken permissionToken) {
                 permissionToken.continuePermissionRequest();
             }
         }).check();
-    }*/
+    }
 
 
 }
